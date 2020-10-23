54601ee02f25b3b2761669f889f167288bd9df93
Hmrinal <mrinaltyagi1912@gmail.com>
2020-10-23 18:46:03 +0530

    transformations

--- chatbot.py
+++ chatbot.py
@@ -10,29 +10,36 @@ engine.say("Hello")
 engine.runAndWait()
 
 #voice input speech recognition
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    audio= r.listen(source)
-    order= r.recognize_google(audio,language="en-in",show_all=True)
+def check():
+    
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio= r.listen(source)
+        order= r.recognize_google(audio,language="en-in",show_all=True)
+    e2.insert(tk.END,order)
+    global x
+    x=order.split(' ')
+    
+
     
 #creating GUI
 root= tk.Tk()
-root.geometry("200*300")
+root.geometry("300x400")
 root.title("Chatbot")
 root.configure(bg="black")
 e1=tk.Text(root)
 l1=tk.Label(root,text="Output:")
-l1.place(x=10,y=5)
-e1.place(x=10,y=35,height=50,width=180)
+l1.place(x=20,y=10)
+e1.place(x=20,y=45,height=50,width=180)
 e2=tk.Text(root)
 
-l=tk.Label(root,text="-----------")
-l.place(x=10,y=90)
+l=tk.Label(root,text="-------------------")
+l.place(x=20,y=100)
 l=tk.Label(root,text="You said:")
-l.place(x=10,y=120)
-e2.place(x=10,y=150,height=30,width=180)
-b1=tk.Button(root,text="Speak",bg="red",fg="white")
-b1.place(x=50,y=200,height=50,width=100)
+l.place(x=20,y=130)
+e2.place(x=20,y=160,height=30,width=180)
+b1=tk.Button(root,text="Speak",bg="red",fg="white",command=check)
+b1.place(x=60,y=210,height=50,width=100)
 
 root.mainloop()
 
